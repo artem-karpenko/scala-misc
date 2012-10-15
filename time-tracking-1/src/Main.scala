6eb2ec1180861ea53a44465bcd663bcb0dd130c4
Artem Karpenko <gooyozh@gmail.com>
2012-10-15 21:41:35 +0300

    added Task class

--- time-tracking-1/src/Main.scala
+++ time-tracking-1/src/Main.scala
@@ -1,3 +1,4 @@
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import org.joda.time.Duration
 import org.joda.time.format.{PeriodFormatterBuilder, PeriodFormatter}
@@ -6,58 +7,33 @@ import org.joda.time.format.{PeriodFormatterBuilder, PeriodFormatter}
  * @author yozh
  */
 object Main {
-   var totalTime: Long = 0
-   var workInProcess = false
-   var startTime: Long = -1
-   var stopTime: Long = -1
-
    val periodFormatter: PeriodFormatter = new PeriodFormatterBuilder()
          .appendHours().appendSuffix(" h").appendSeparator(" ").appendMinutes().appendSuffix(" m")
          .appendSeconds().appendSuffix(" s")
          .toFormatter;
 
    def main(args: Array[String]): Unit = {
+      println("Enter name of task")
+      val task = new Task(readLine())
+
       while (true) {
+         println("Next command")
          val cmd = readLine()
 
          cmd.toLowerCase() match {
-            case "start" => startWork()
-            case "stop" => stopWork()
-            case "exit" => exitWork(); return
+            case "start" => task startWork
+            case "stop" => task stopWork
+            case "exit" => exitWork(task); return
             case _ => println("Unknown command: " + cmd)
          }
       }
    }
 
-   def startWork() = {
-      if (workInProcess) {
-         println("Error: work already started")
+   def exitWork(task: Task) = {
+      if (task.workInProgress) {
+         task.stopWork()
       }
-      else {
-         startTime = System.currentTimeMillis()
-         workInProcess = true
-         println("Work started")
-      }
-   }
-
-   def stopWork() = {
-      if (!workInProcess) {
-         println("Error: work not started")
-      }
-      else {
-         stopTime = System.currentTimeMillis()
-         workInProcess = false
-
-         val timeWorked = stopTime - startTime
-         totalTime += timeWorked
-         println("Work stopped, time worked: " + periodFormatter.print(new Duration(timeWorked).toPeriod()))
-      }
-   }
-
-   def exitWork() = {
-      if (workInProcess) {
-         stopWork()
-      }
-      println("Exiting, total time worked: " + periodFormatter.print(new Duration(totalTime).toPeriod()))
+      println("Exiting, total time worked on task " + task.name + ": " +
+            periodFormatter.print(new Duration(task.totalTime).toPeriod()))
    }
 }
---
+++ time-tracking-1/src/Task.scala
@@ -0,0 +1,40 @@
+import org.joda.time.Duration
+
+/**
+ * @author Artem
+ */
+class Task(val name: String) {
+   var startTime = -1L
+   var workInProgress = false
+   var totalTime = 0L
+
+   /**
+    * Start work on this task
+    * @return true if work was started, false otherwise
+    */
+   def startWork() = {
+      var result = false
+      if (!workInProgress) {
+         startTime = System.currentTimeMillis()
+         workInProgress = true
+         result = true
+      }
+      result
+   }
+
+   /**
+    * Stop work on this task
+    * @return true if work was stopped, false otherwise
+    */
+   def stopWork() = {
+      var result = false
+      if (workInProgress) {
+         val stopTime = System.currentTimeMillis()
+         workInProgress = false
+
+         val timeWorked = stopTime - startTime
+         totalTime += timeWorked
+      }
+      result
+   }
+}
