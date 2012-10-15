2316a3f4c4ad7c14ed14896cd0cd71749dd0cf22
Artem Karpenko <gooyozh@gmail.com>
2012-10-15 21:42:54 +0300

    some stuff

---
+++ idea-1/src/Literals.scala
@@ -0,0 +1,9 @@
+/**
+ * @author yozh
+ */
+object Literals {
+   def main(args: Array[String]) {
+      println(1.1.toString)
+      println('asd)
+   }
+}
---
+++ idea-1/src/Tuples.scala
@@ -0,0 +1,14 @@
+/**
+ * @author yozh
+ */
+
+println((1, "hello"))
+
+val v1 = (1, 2, "big")
+val (_, _, t1) = v1
+println(t1)
+
+println(v1._2)
+
+println(1 -> "One")
+println(1 -> "One" -> "2")
